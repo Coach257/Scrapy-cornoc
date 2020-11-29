cd668ca326cae7f1b29eaec07723c33e396b6cb3
Coach257 <45021066+Coach257@users.noreply.github.com>
2020-11-29 12:03:39 +0800

    finish item

--- cornoc_scrapy/cornoc_scrapy/items.py
+++ cornoc_scrapy/cornoc_scrapy/items.py
@@ -9,4 +9,25 @@ import scrapy
 class CornocScrapyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    #航空公司图片
+    img = scrapy.Field()
+    #航空公司地址
+    href = scrapy.Field()
+    #航班号
+    number = scrapy.Field()
+    #始发地、目的地
+    city = scrapy.Field()
+    #停机楼
+    terminal = scrapy.Field()
+    #预计
+    expected = scrapy.Field()
+    #实际
+    actual = scrapy.Field()
+    #状态
+    state = scrapy.Field()
+
+    #现在时间(爬取时间)
+    nowtime = scrapy.Field()
+    
+    #pass
